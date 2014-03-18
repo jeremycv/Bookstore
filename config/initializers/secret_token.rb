aa7b1b875ea77e7e0372df682978b31dfc6b8b39
Jeremy Vandersay <jeremyv@tpg.com.au>
2014-03-18 17:53:49 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstorev32::Application.config.secret_token = 'c8dc71a7acf47384974cb8e7559f2917f49da785593e83b2d5af1e6e10dcb45c3db7d89ae276a777f84df71b377fdc30afbfdc517ef147b0dce1e00efff285e9'
